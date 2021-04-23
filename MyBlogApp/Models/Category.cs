cbb22f3262aa133e00e92d8811d3b230fd35046c
Seiya_Satoh <satohseiya0312@gmail.com>
2021-04-23 09:30:20 +0900

    Model作成

--- MyBlogApp/Models/Category.cs
+++ MyBlogApp/Models/Category.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,16 @@ namespace MyBlogApp.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [DisplayName("カテゴリー")]
+        public string CategoryName { get; set; }
+
+        [DisplayName("件数")]
+        public int Count { get; set; }
+
+        public virtual ICollection<Article> Articles { get; set; }
     }
 }
