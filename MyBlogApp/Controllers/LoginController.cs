ce05010f106f95b63baf0fae98596552e4a59db0
Seiya_Satoh <satohseiya0312@gmail.com>
2021-04-26 10:29:55 +0900

    認証機能の実装

---
+++ MyBlogApp/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlogApp.Controllers
+{   
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        readonly private CustomMembershipProvider membershipProvider = new CustomMembershipProvider();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "UserName,Password")] LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(this.membershipProvider.validateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Articels");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Articles");
+        }
+
+
+    }
+}
